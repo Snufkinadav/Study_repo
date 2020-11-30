7dbe42749bdc58d836f227936d92c67d6c99efe0
Snufkin <dadkinsnadav@outlook.com>
2020-11-30 23:21:45 +0000

    FIRST GAME

--- Study.py
+++ Study.py
@@ -1,10 +1,24 @@
 from random import shuffle
- 
-def shuffle_l(x):
-     shuffle(x)
-     return x[0]
 
-mylist = list(range(1,11))
+def shuffle_list(i):
+    shuffle(i)
+    return i
 
-x = shuffle_l(mylist)
-print(x)
+def player_guess():
+    guess = ''
+    while guess not in ['0','1','2']:
+        guess = input("Hey you!\nLet's play a game,\nChoose a number 0,1 or 2: ")
+        return int(guess)
+
+def check_guess(mylist,guess):
+    if mylist[guess] == 'O':
+        print("Well Done!, you are right!")
+        print(mylist)
+    else:
+        print("Nope, this is not what I chose!")
+        print(f"my choise was {mylist}")
+
+def_list = ['','O','']
+mix_list = shuffle_list(def_list)
+guess = player_guess()
+check_guess(mix_list,guess)
