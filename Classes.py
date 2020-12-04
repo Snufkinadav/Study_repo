05f87a68e719031176b92059144fc54f06426749
Snufkin <dadkinsnadav@outlook.com>
2020-12-04 00:07:49 +0000

    End_of_class

--- Classes.py
+++ Classes.py
@@ -2,30 +2,45 @@ import random
 
 class Employee:
     """Createing User class, follwing yt=https://www.youtube.com/watch?v=ZDa-Z5JzLYM"""
+    pay_raise = 1.04
+    pay = 0
+    num__of_emps = 0
+
     def __init__(self,first,last,pay):
         self.first = first
         self.last = last
         self.pay = pay
         self.email = first + '.' + last +'@company.com'
+        Employee.num__of_emps += 1
         #From here it's my own try and error
         
 
-    def pay_raise(self):
-        self.pay = int(self.pay *1.10)
+    def apply_raise(self):
+        self.pay = int(self.pay * self.pay_raise)
         #From here it's my own try and error
     
     def full_name(self):
         print(f'{self.first} {self.last}')
         #From here it's my own try and error
 
-emp_1 = Employee('Dor','Nadav',50000)
-emp_2 = Employee('Graham','Adkins-Nadav',60000)
+user1 = Employee('Dor','Nadav',50000)
+user2 = Employee('Graham','Adkins-Nadav',60000)
 
-emp_1.pay_raise()
+print(Employee.num__of_emps)
 
-print(emp_1.pay)
-# print(emp_1.full_name())
-# print(emp_1.email)
-# print(emp_2.email)
-# print(emp_1.full_name())
-# print(emp_2.full_name())
+
+# user1.pay_raise = 1.06
+
+# print(Employee.pay_raise)
+# print(user1.pay_raise)
+# print(user2.pay_raise)
+
+# print(Employee.pay)
+# print(user1.pay)
+# print(user2.pay)
+# print(user1.pay)
+# print(user1.full_name())
+# print(user1.email)
+# print(user2.email)
+# print(user1.full_name())
+# print(user2.full_name())
